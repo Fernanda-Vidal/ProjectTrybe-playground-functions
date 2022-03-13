bd45160580d67e899ee1937be1a9a6621f1a03cd
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-12 22:00:00 -0300

    Corrigindo exercício 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,11 @@
 // Desafio 11
 function generatePhoneNumber(numeroTel) {
-return ('(' + numeroTel[0] + numeroTel[1] + ')' + numeroTel[2] + numeroTel[3] + numeroTel [4] + numeroTel[5] + numeroTel[6] + '-' + numeroTel[7] + numeroTel[8] + numeroTel[9] + numeroTel[10]);
-
+  let novoNumero = numeroTel;
+  if (novoNumero.length ===11){
+  return ('(' + numeroTel[0] + numeroTel[1] + ')' + numeroTel[2] + numeroTel[3] + numeroTel [4] + numeroTel[5] + numeroTel[6] + '-' + numeroTel[7] + numeroTel[8] + numeroTel[9] + numeroTel[10]);
+  }
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 
 // Desafio 12
 function triangleCheck() {
