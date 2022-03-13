a77c13f4692964a276568993306bb2efeed5f587
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-12 21:38:04 -0300

    Começa a corrigir exercício 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,24 +1,9 @@
 // Desafio 11
 function generatePhoneNumber(numeroTel) {
-  let numeroTelefone = [];
-  let prefixo = [];
-  for (number in numeroTel) {
-    numeroTelefone += numeroTel[number];
-  }
-  for (let i = 0; i < numeroTelefone[1]; i += 1) {
-    prefixo += numeroTelefone[i];
-  }
-  let phoneNumber = ['(' + prefixo + ')' + numeroTelefone]
+return ('(' + numeroTel[0] + numeroTel[1] + ')' + numeroTel[2] + numeroTel[3] + numeroTel [4] + numeroTel[5] + numeroTel[6] + '-' + numeroTel[7] + numeroTel[8] + numeroTel[9] + numeroTel[10]);
 
-  return phoneNumber;
 }
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
-
-// let luzNaVida = [numeroTel];
-// if (luzNaVida.length != 11){
-//   return "Array com tamanho incorreto";
-// }
-// else {
 
 // Desafio 12
 function triangleCheck() {
