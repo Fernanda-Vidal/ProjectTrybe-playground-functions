f0d330993b3c128f15d22e073dcbcc3b48150043
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-17 18:57:55 -0300

    Adiciona requisito 12.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,33 +1,43 @@
 // Desafio 11
+function contador(element1, element2) {
+  var result = 0;
+  for (i = 0; i < element1.length; i += 1) {
+    if (element1[i] == element2) { result += 1 }
+  }
+  return result;
+}
+
 function generatePhoneNumber(numeroTel) {
   let novoNumero = numeroTel;
 
-  function contador(element1, element2){
-      var result = 0;
-      for(i = 0; i < element1.length; i += 1){
-          if(element1[i] == element2){result += 1}
-      }
-      return result;
-  }
-
   if (novoNumero.length !== 11) {
-      return "Array com tamanho incorreto.";
+    return "Array com tamanho incorreto.";
   }
   for (numero in numeroTel) {
-          if (numeroTel[numero] < 0 || numeroTel[numero] > 9) {
-          return "não é possível gerar um número de telefone com esses valores"
-      }
-      else if (contador(numeroTel, numeroTel[numero]) >= 3){
-          return "não é possível gerar um número de telefone com esses valores"
-      }      
-  }
-  return ('(' + numeroTel[0] + numeroTel[1] + ') ' + numeroTel[2] + numeroTel[3] + numeroTel [4] + numeroTel[5] + numeroTel[6] + '-' + numeroTel[7] + numeroTel[8] + numeroTel[9] + numeroTel[10]);
+    if (numeroTel[numero] < 0 || numeroTel[numero] > 9) {
+      return "não é possível gerar um número de telefone com esses valores"
     }
+    else if (contador(numeroTel, numeroTel[numero]) >= 3) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+  }
+  return ('(' + numeroTel[0] + numeroTel[1] + ') ' + numeroTel[2] + numeroTel[3] + numeroTel[4] + numeroTel[5] + numeroTel[6] + '-' + numeroTel[7] + numeroTel[8] + numeroTel[9] + numeroTel[10]);
+}
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA > lineB + lineC || lineA < lineB - lineC) {
+    return false;
+  }
+  if (lineB > lineA + lineC || lineB < lineA - lineC ) {
+    return false;
+  }
+  if (lineC > lineA + lineB || lineC < lineA - lineB) {
+    return false;
+  }
+  return true;
 }
+
 
 // Desafio 13
 function hydrate() {
